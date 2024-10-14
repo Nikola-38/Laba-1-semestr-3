e71d3f0a8d43908ed5a7bac306862e9cef8d4ceb
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-10-14 22:18:26 +0700

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,43 +1,48 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+
 using namespace std;
 
-struct Node {
+struct NodeS {
     string data;
-    Node* next;
+    NodeS* next;
 };
 
 struct Stack {
-    Node* head = nullptr; // Инициализация стека
+    NodeS* head = nullptr;
+
 };
 
 // Добавление элемента в стек (push)
-void push(Stack* stack, const string& data) {
-    Node* new_node = new Node{data, nullptr};
-    new_node->next = stack->head;
-    stack->head = new_node;
+void pushS(Stack* stack, const string& data) {
+    NodeS* new_NodeS = new NodeS{data, nullptr};
+    new_NodeS->next = stack->head;
+    stack->head = new_NodeS;
 }
 
 // Удаление элемента из стека (pop)
-void pop(Stack* stack) {
+string popS(Stack* stack) {
     if (stack->head == nullptr) {
         cout << "Стек пуст!" << endl;
-        return;
+        return ""; // Возвращаем пустую строку, если стек пуст
     }
 
-    Node* new_head = stack->head->next;
-    delete stack->head;
-    stack->head = new_head;
+    NodeS* temp = stack->head;
+    string data = temp->data; // Сохраняем данные удаляемого элемента
+    stack->head = stack->head->next; // Обновляем голову стека
+    delete temp; // Освобождаем память
+    return data; // Возвращаем данные
 }
 
 // Функция для вывода всех элементов стека
-void printStack(Stack* stack) {
+void printStackS(Stack* stack) {
     if (stack->head == nullptr) {
         cout << "Стек пуст!" << endl;
         return;
     }
 
-    Node* current = stack->head;
+    NodeS* current = stack->head;
     
     
     // Вывод элементов от верхнего к нижнему
@@ -51,25 +56,24 @@ void printStack(Stack* stack) {
 // Освобождение памяти стека
 void clearStack(Stack* stack) {
     while (stack->head) {
-        pop(stack);
+        popS(stack);
     }
 }
 
-int main() {
-    Stack stack;
-
-    // Пример использования стека
-    push(&stack, "Копатыч");
-    push(&stack, "Нюша");
-    push(&stack, "Шрэк");
-
-    cout << "Все элементы Stack: ";
-    printStack(&stack); 
-
-    pop(&stack);
-    cout << "Элементы Stack после удаления: ";
-    printStack(&stack); 
-
-    clearStack(&stack); // Освобождение памяти
-    return 0;
+// Функция для записи стека в файл
+void writeToFileS(const Stack& stack, const string& filename) {
+    ofstream file(filename);
+    if (!file) {
+        cout << "Не удалось открыть файл для записи.\n";
+        return;
+    }
+    NodeS* temp = stack.head;
+    while (temp) {
+        file << temp->data << endl;
+        temp = temp->next;
+    }
+    cout << "Данные успешно записаны в файл.\n";
+    file.close();
 }
+
+
