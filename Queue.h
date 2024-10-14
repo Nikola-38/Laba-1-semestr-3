6d3e6bc949114edd92cb7c37762622e4680b7e88
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-10-14 22:20:45 +0700

    Create Queue.h

---
+++ Queue.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+using namespace std;
+
+struct NodeQ {
+    int data;
+    NodeQ* next;
+};
+
+struct Queue {
+    NodeQ* head;
+    NodeQ* tail;
+
+    Queue() : head(nullptr), tail(nullptr) {}
+};
+
+void pushQ(Queue& queue, int value);
+int popQ(Queue& queue);
+void printQueue(const Queue& queue);
+void clearQueue(Queue& queue) ;
+void writeToFileQ(const Queue& queue, const string& filename);
+void readFromFileQ(Queue& queue, const string& filename);
+
+
