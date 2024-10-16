68880aa8eb41ef0aac873a3e6946c497990f03b4
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-10-16 13:30:59 +0700

    Update array.h

--- array.h
+++ array.h
@@ -1,21 +1,20 @@
-#pragma once
 #include <iostream>
 using namespace std;
 
-struct ArrayNode {
-    int data;
-    ArrayNode* next;
-    ArrayNode* previous;
+// Структура для массива
+struct Array {
+    int* data;       // Указатель на массив
+    int size;        // Текущий размер массива
+    int capacity;    // Максимальная вместимость массива
 };
 
-void appendArray(ArrayNode*& head, int value);
-void insertArray(ArrayNode*& head, int index, int value);
-int getArray(ArrayNode* head, int index);
-void removeArray(ArrayNode*& head, int index);
-void replaceArray(ArrayNode* head, int index, int value);
-int sizeArray(ArrayNode* head);
-void printArray(ArrayNode* head);
-void clearArray(ArrayNode*& head);
-void writeToFileArray(ArrayNode* head, const string& filename);
-void readFromFileArray(ArrayNode*& head, const string& filename);
-bool containsArray(ArrayNode* head, int value);
+
+void Ainit(Array& arr, int cap);
+void Adestroy(Array& arr);
+void Aappend(Array& arr, int value);
+void Ainsert(Array& arr, int index, int value);
+int Aget(const Array& arr, int index);
+void Aremove(Array& arr, int index);
+void Areplace(Array& arr, int index, int value);
+int Alength(const Array& arr);
+void printArray(const Array& arr);
